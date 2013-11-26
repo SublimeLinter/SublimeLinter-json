4390b67ed22408765b6c5bead21acf40a90efbac
Aparajita Fishman <aparajita@aparajita.com>
2013-11-26 11:23:31 -0500

    PEP257 docstrings

--- json.py
+++ json.py
@@ -8,20 +8,25 @@
 # License: MIT
 #
 
+"""This module exports the JSON plugin linter class."""
+
 import json
 
 from SublimeLinter.lint import Linter
 
 
 class JSON(Linter):
+
+    """Provides an interface to json.loads()."""
+
     language = 'json'
     cmd = None
     regex = r'^(?P<message>.+):\s*line (?P<line>\d+) column (?P<col>\d+)'
 
     def run(self, cmd, code):
+        """Attempt to parse code as JSON, return '' if it succeeds, the error message if it fails."""
         try:
             json.loads(code)
+            return ''
         except ValueError as err:
             return str(err)
-
-        return ''
