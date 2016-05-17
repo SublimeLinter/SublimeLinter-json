fb0878b94c5b5abbf64a09f69e28d6aba1b5b16d
Blake Grotewold <hello@grotewold.me>
2016-05-17 01:20:07 -0400

    Copyright bump

--- linter.py
+++ linter.py
@@ -3,7 +3,8 @@
 # Linter for SublimeLinter3, a code checking framework for Sublime Text 3
 #
 # Written by Aparajita Fishman
-# Copyright (c) 2013 Aparajita Fishman
+# Copyright (c) 2015-2016 The SublimeLinter Community
+# Copyright (c) 2013-2014 Aparajita Fishman
 #
 # License: MIT
 #
@@ -19,7 +20,6 @@ from SublimeLinter.lint import Linter
 
 
 class JSON(Linter):
-
     """Provides an interface to json.loads()."""
 
     syntax = 'json'
