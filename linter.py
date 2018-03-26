f649ee3697c512ea77375d6ca91f04646f6f7e27
Koen Lageveen <koen.lageveen@me.com>
2018-03-26 13:16:17 +0200

    use selectors + some cleanup

--- linter.py
+++ linter.py
@@ -1,16 +1,3 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Written by Aparajita Fishman
-# Copyright (c) 2015-2016 The SublimeLinter Community
-# Copyright (c) 2013-2014 Aparajita Fishman
-#
-# License: MIT
-#
-
-"""This module exports the JSON plugin linter class."""
-
 import json
 import os.path
 import re
@@ -20,32 +7,14 @@ from SublimeLinter.lint import Linter
 
 
 class JSON(Linter):
-    """Provides an interface to json.loads()."""
-
-    syntax = 'json'
     cmd = None
     loose_regex = re.compile(r'^.+: (?P<message>.+) in \(data\):(?P<line>\d+):(?P<col>\d+)')
     strict_regex = re.compile(r'^(?P<message>.+):\s*line (?P<line>\d+) column (?P<col>\d+)')
     regex = loose_regex
     defaults = {
+        'selector': 'source.json',
         'strict': True
     }
-
-    extensions = [
-        '.sublime-build',
-        '.sublime-color-scheme',
-        '.sublime-commands',
-        '.sublime-completions',
-        '.sublime-keymap',
-        '.sublime-menu',
-        '.sublime_metrics',
-        '.sublime-mousemap',
-        '.sublime-project',
-        '.sublime_session',
-        '.sublime-settings',
-        '.sublime-theme',
-        '.sublime-workspace',
-    ]
 
     def run(self, cmd, code):
         """
@@ -54,19 +23,19 @@ class JSON(Linter):
         Returns '' if it succeeds, the error message if it fails.
         Use ST's loose parser for its setting files, or when specified.
         """
-        is_sublime_file = os.path.splitext(self.filename)[1] in self.extensions
+        is_sublime_file = os.path.splitext(self.filename)[1].startswith('.sublime-')
 
-        if Linter.get_view_settings(self).get('strict') and not is_sublime_file:
+        if self.get_view_settings().get('strict') and not is_sublime_file:
             strict = True
         else:
             strict = False
 
         try:
             if strict:
-                self.__class__.regex = self.strict_regex
+                self.regex = self.strict_regex
                 json.loads(code)
             else:
-                self.__class__.regex = self.loose_regex
+                self.regex = self.loose_regex
                 sublime.decode_value(code)
 
             return ''
